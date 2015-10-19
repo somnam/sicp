fca09eaf79bc8e05c5250a6e3bd49f6c70bf4d92
Tomek Miodek <tomek.miodek@gmail.com>
2015-10-19 20:29:11 +0200

    3.3.2 Representing queues

--- 033_mutable_data.scm
+++ 033_mutable_data.scm
@@ -15,11 +15,11 @@
 ;; Basic operations on pairs - cons, car, cdr are incapable of modifying list
 ;; structure. The same is true of the list operations we have used so far. To
 ;; modify list structures we need new operations.
-;; The primitive mutators for pairs are set-car! and set-cdr!. THe first one 
+;; The primitive mutators for pairs are set-car! and set-cdr!. The first one 
 ;; takes two arguments, first of which must be a pair. It replaces the car
 ;; pointer of this pair by the second argument of set-car!.
 ;; x and y are bound to given lists:
-(define x (list (list 'a 'b) 'c 'd))
+(define x (list (list 'a 'b) 'c 'd 'g 'h 'i))
 (say "x:" x)
 (define y (list 'e 'f))
 (say "y:" y)
@@ -33,7 +33,7 @@
 ;; The list (c d) is now detached from the structure.
 
 ;; We could implement cons operation in terms of two mutators, together with
-;; a procedure get-new-pair which terurns a new pair that is not part of any
+;; a procedure get-new-pair which returns a new pair that is not part of any
 ;; existing list structure.
 (define (get-new-pair) (cons '() '()))
 (define (cons2 x y)
@@ -44,9 +44,15 @@
 (say "cons" (cons2 1 2))
 
 ;; Exercise 3.12
-;; The procedure append! is a mutator, that appends two lists by splicing them
-;; together. It modifies the final pair of x so that its cdr is now y.
-(define (last pair x)
+;; The procedure for appending two lists is as follows:
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+;; The procedure append! is similar to append, but it's a mutator, that appends
+;; two lists by splicing them together. It modifies the final pair of x so that
+;; its cdr is now y.
+(define (last-pair x)
   (if (null? (cdr x))
       x
       (last-pair (cdr x))))
@@ -98,14 +104,14 @@
 ;; Even though z1 and z2 are "the same" structure, applying set-to-doge! to them
 ;; yields different results. With z1 altering the car also alters the cdr, 
 ;; because they are the same pair. With z2 only the car is modified:
+;; One way to detect sharing in lists is to use the eq? predicate which tests
+;; if two symbols point to the same structure.
 (say "eq? z1" (eq? (car z1) (cdr z1)))
 (say "eq? z2" (eq? (car z2) (cdr z2)))
 (set-to-doge! z1)
 (say "z1" z1)
 (set-to-doge! z2)
 (say "z2" z2)
-;; One way to detect sharing in lists is to use the eq? predicate which tests
-;; if two symbols point to the same structure.
 
 ;; Exercise 3.17
 (define (in-list list elem)
@@ -118,12 +124,18 @@
 (define (count-pairs x)
   (let ((encountered '()))
     (define (helper x)
+      ;; Current item is the cdr of the last element in list or was already
+      ;; marked as encountered when traversing the pairs list.
       (if (or (not (pair? x)) (memq x encountered))
           0
+          ;; The begin block is a wrapper for evaluating multiple expressions
+          ;; in a "true" or "false" if block.
           (begin
+            ;; Append newli encountered item to list of encountered elements.
             (set! encountered (cons x encountered))
             (+ (helper (car x))
                (helper (cdr x))
+               ;; Start with 1 for the input list pair of car and cdr.
                1))))
     (helper x)))
 (say "count-pairs z1" (count-pairs z1))
@@ -136,4 +148,96 @@
 ;; TODO
 
 ;; 3.3.2 Representing queues
-;; 
+;; The mutators set-car! and set-cdr! enable us to use pairs when constructing
+;; data sctructures impossible to build with cons, car and cdr alone.
+;; One of them is a queue. It's a sequence in which items are inserted at one
+;; end (rear) and deleted from the other end (front) - it's a FIFO type queue.
+;; We can define it using the following operations:
+;; * a constructor (make-queue) returns an empty queue
+;; * a selector testing if the queue is empty (empty-queue? <queue>)
+;; * a selector returning the object at the front of the queue, throwing an
+;;   error if it's empty
+;; * a mutator (insert-queue! <queue> <item>) inserting the item at the end
+;;   of the queue and returing it
+;; * a mutator (delete-queue! <queue>) removing the item at the front of the
+;;   queue and returing it, throws an error if the queue is empty beforehand
+
+;; The implementation will use a list representation with a simple modification
+;; which allows the queue operations to be implemeted so that they require O(1)
+;; steps. The difficulty in the list representation arises from the need to scan
+;; to find the end of the list. We only have a pointer to the beginning of the
+;; list. The modification that avoids this drawback is an additional pointer to
+;; the final part of the list. When inserting an item, we can use this pointer
+;; to avoid scanning the whole list.
+;; A queue is represented as a pair of pointers - front-ptr and rear-ptr.
+;; We can use cons to combine the two pointers, thus creating a queue.
+;; Queue procedures:
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+;; Queue definition:
+(define (make-queue) (cons '() '()))
+;; To select the item at the front of the queue we return the car of the pair
+;; indicated by the front pointer:
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "Function called with an empty queue" queue)
+      (car (front-ptr queue))))
+;; Insert an item in a queue:
+;; - create a new pair whose car is the item to be inserted, and cdr is empty
+;; - if the queue is empty we set the front and rear pointers to this item
+;; - if not, we modfy the final pair in the queue to point to the new pair
+;;   and set the rear-ptr to it
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+            (set-cdr! (rear-ptr queue) new-pair)
+            (set-rear-ptr! queue new-pair)
+            queue))))
+;; To delete an item at the front of the queue we modify the front-ptr so that
+;; it points to the second item in the queue, accessible by the first items cdr.
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "delete-queue! called with an empty queue." queue))
+        (else
+          (set-front-ptr! queue (cdr (front-ptr queue)))
+          queue)))
+;; Exercise 3.21
+(define (print-queue queue)
+  (define (printer queue-item)
+    (cond ((null? queue-item)
+           (newline))
+          (else
+            (display (car queue-item))
+            (printer (cdr queue-item)))))
+  (if (empty-queue? queue)
+      (begin (display '()) (newline))
+      (printer (front-ptr queue))))
+
+;; Test
+(define q1 (make-queue))
+(say "queue" q1)
+(insert-queue! q1 'a)
+(display "insert-queue! ")
+(print-queue q1)
+(insert-queue! q1 'b)
+(display "insert-queue! ")
+(print-queue q1)
+(insert-queue! q1 'c)
+(display "insert-queue! ")
+(print-queue q1)
+(delete-queue! q1)
+(display "delete-queue! ")
+(print-queue q1)
+(delete-queue! q1)
+(display "delete-queue! ")
+(print-queue q1)
+(delete-queue! q1)
+(display "delete-queue! ")
+(print-queue q1)
